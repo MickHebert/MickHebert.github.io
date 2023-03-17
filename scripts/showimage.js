51ab06fc8494a979f03744238cff93daed5fd261
Michael Hebert <42976479+MickHebert@users.noreply.github.com>
2023-03-17 00:47:37 -0500

    Initial website commit

---
+++ scripts/showimage.js
@@ -0,0 +1,10 @@
+const image_show = document.createElement(show_ed);
+
+image_show.textContent = "<strong>corn-dog?</strong>";
+
+document.body.appendChild(image_show);
+
+image_show.onclick = function() {
+    const ed_image = document.createElement("edimg");
+}
+
